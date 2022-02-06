62e3052c9440fc0de7094f526ac8d052f637a50b
jpc0023 <jpc0023@uah.edu>
2022-02-06 16:41:00 -0600

    finished

--- index.js
+++ index.js
@@ -1,13 +1,127 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+      validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+              console.log('Please enter your project title!');
+            return false;
+          }
+      }
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a short description of this project.",
+      validate: nameInput => {
+          if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter a description');
+              return false;
+          }
+      }
+    },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please select the license you used for this project.",
+      choices: [
+        "None",
+        "GPL 3",
+        "MIT",
+        "Apache 2"
+      ],
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will your project be used?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'List any guidelines others must follow to contribute.',
+        validate: contributionsInput => {
+            if (contributionsInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines');
+                return false;
+            }
+        }
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "List the tests used for your project.",
+    },
+    {
+      type: "input",
+      name: "author",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "userName",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "userEmail",
+      message: "What is your GitHub email?",
+    },
+    {
+      type: "input",
+      name: "URL",
+      message: "What is the URL of the deployed project? (include http://)",
+    },
+    {
+      type: "input",
+      name: "repo",
+      message: "What is the URL of the repository? (include http://)",
+    }
+  ];
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/README.md', fileContent, err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'File created!'
+        });
+      });
+    });
+  };
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions)
+        .then(function(answer) {
+            console.log(answer);
+        var fileContent = generateMarkdown(answer);
+        writeFile(fileContent)
+        });
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  let badge = '';
+  if(license === 'MIT') {
+      badge = '![NPM](https://img.shields.io/npm/l/inquirer)'
+  } else if (license === 'Apache 2') {
+      badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  } else if (license === 'GPL 3') {
+      badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+  } else {
+    badge = ""
+  }
+  return badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+    if(license === 'MIT') {
+      licenseLink = 'https://choosealicense.com/licenses/mit/'
+    } else if (license === 'Apache 2') {
+      licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
+    } else if (license === 'GPL 3') {
+      licenseLink = 'https://www.gnu.org/licenses'
+    } else {
+      licenseLink = ""
+    }
+    return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection =
+    `License: ${license} `
+  }
+  return licenseSection;
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} - ${data.author}
 
+  Link to the live project - [${data.title}](${data.URL})  
+  Link to the repository - [${data.title}](${data.repo})
+
+  ## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
+  ### ${renderLicenseLink(data.license)}
+
+
+  ## Table of Contents:
+  ###  * [Installation](#installation)
+  ###  * [Description](#description)
+  ###  * [Usage](#usage)
+  ###  * [License](#license)
+  ###  * [Contributing](#contributions)
+  ###  * [Tests](#tests)
+  ###  * [Questions](#questions)
+
+  ## Installation:
+  ### You must install the following for this app to function:
+  ${data.installation}
+
+  ## Description:
+  ${data.description}
+
+  ## Usage:
+  ${data.usage}
+
+  ## Contribution guidelines:
+  ${data.contributions}
+
+  ## Tests:
+  ### Run the following commands in your terminal to test this app:
+  ${data.tests}
+
+  ## Questions:
+  ### If you have any questions, you may contact me at either
+  Github: [https://github.com/${data.userName}](https://github.com/${data.userName})
+  or
+  Email: [${data.userEmail}](mailto:${data.userEmail})
 `;
 }
 
